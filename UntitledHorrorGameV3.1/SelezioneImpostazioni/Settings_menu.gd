59369f6d56a08d9f969748a9a7fc0f0b601fd856
TTVTheCats <153516781+TTVTheCats@users.noreply.github.com>
2024-03-02 12:02:39 +0100

    Version 3.1
    Added menu background, and others fixes

---
+++ UntitledHorrorGameV3.1/SelezioneImpostazioni/Settings_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _on_video_pressed(): 
+	get_tree().change_scene_to_file("uid://c428whyct2yye")
+
+func _on_audio_pressed(): 
+	get_tree().change_scene_to_file("uid://vhh3mhi7ldyo")
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("uid://bj26pefpj6bee") #Menu Iniziale
+
